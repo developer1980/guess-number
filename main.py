c9f582ff3a0b28a4d5dfab26da8f3fd98e80571c
developer1980 <farhad_usa2006@yahoo.co.uk>
2026-01-03 22:37:06 +0330

    version 0.0

--- main.py
+++ main.py
@@ -6,12 +6,10 @@ number=random.randrange(1,100)
 for i in range(0,100):
     print("cpu choosen " + str(number))
     user= input("choose a number between 1 an 100\nEnter a number: ")
-    if int(user)== number:
-        print("YOU WIN")
-        break
-    elif int(user)<number:
+    if int(user)<number:
         print("you number is smaller than cpu")
-        user= input("choose a number between 1 an 100\nEnter a number: ")
     elif int(user)>number:
         print("you number is biger than cpu")
-        user= input("choose a number between 1 an 100\nEnter a number: ")
+    elif int(user)== number:
+        print("YOU WIN")
+        break

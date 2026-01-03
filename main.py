783e265e6ce581ad1dd78ed3e026d82b7614c4bc
developer1980 <farhad_usa2006@yahoo.co.uk>
2026-01-03 22:44:25 +0330

    get number number of guess

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ print("Hello,Welcome to my tiny app")
 print("Computer choose a number between 1 and 100,you find it")
 number=random.randrange(1,100)
 for i in range(0,100):
-    print("cpu choosen " + str(number))
+    #print("cpu choosen " + str(number))  //cpu hint
     user= input("choose a number between 1 an 100\nEnter a number: ")
     if int(user)<number:
         print("you number is smaller than cpu")
     elif int(user)>number:
         print("you number is biger than cpu")
     elif int(user)== number:
-        print("YOU WIN")
+        print("YOU WIN with TRY " +str(i+1))
         break

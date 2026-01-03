7208c7ed77186925411f317b2975081d5f5b2a20
developer1980 <farhad_usa2006@yahoo.co.uk>
2026-01-03 22:09:34 +0330

    user enter number

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ import random
 print("Hello,Welcome to my tiny app")
 print("Computer choose a number between 1 and 100,you find it")
 number=random.randrange(1,100)
+
+user= input("choose a number between 1 an 100\nEnter a number: ")
+if int(user)== number:
+    print("YOU WIN")
+elif int(user)<number:
+    print("you number is smaller than cpu")
+    user= input("choose a number between 1 an 100\nEnter a number: ")
+elif int(user)>number:
+    print("you number is biger than cpu")
+    user= input("choose a number between 1 an 100\nEnter a number: ")
